6b57b0acccb63116ff78c3689342b97b09c22984
kelechistack12345 <unachukwukelechi7@gmail.com>
2026-06-06 16:10:54 +0100

    w3  assignment file

---
+++ Assigment/W3/Week 3.sql
@@ -0,0 +1,53 @@
+/*Week 3 Assignment: Business Performance
+Analysis*/
+
+/*Question 1 – Key Revenue Drivers
+Which customers contribute the most revenue to the business, and how frequently do they
+purchase?*/
+
+SELECT
+    B.NAME,
+    COUNT(B.NAME) AS NO_SALES,
+    SUM(A.TOTAL_AMT_USD) AS VALUE
+FROM ORDERS A
+JOIN ACCOUNTS B
+    ON A.ACCOUNT_ID = B.ID
+GROUP BY B.NAME
+ORDER BY VALUE DESC;
+
+/*Question 3 – Regional Business Review*/
+
+SELECT
+    D.NAME,
+    SUM(A.TOTAL_AMT_USD) AS VALUE,
+    COUNT(B.NAME) AS CUSTOMER_ACQUISITION,
+    AVG(A.TOTAL_AMT_USD) AS AVERAGE_ORDER_VALUE
+FROM ORDERS A
+JOIN ACCOUNTS B
+    ON A.ACCOUNT_ID = B.ID
+JOIN SALES_REPS C
+    ON B.SALES_REP_ID = C.ID
+JOIN REGION D
+    ON C.REGION_ID = D.ID
+GROUP BY D.NAME
+ORDER BY AVERAGE_ORDER_VALUE DESC;
+
+/*Question 5 – Customer Segmentation*/
+
+WITH CUSTOMER_REVENUE AS (
+    SELECT
+        B.NAME,
+        SUM(A.TOTAL_AMT_USD) AS TOTAL_REVENUE
+    FROM ORDERS A
+    JOIN ACCOUNTS B
+        ON A.ACCOUNT_ID = B.ID
+    GROUP BY B.NAME
+)
+SELECT *
+FROM CUSTOMER_REVENUE
+WHERE TOTAL_REVENUE >
+(
+    SELECT AVG(TOTAL_REVENUE)
+    FROM CUSTOMER_REVENUE
+)
+ORDER BY TOTAL_REVENUE DESC;
