77988c8273defe5d5f3528ca9840ee11c77f625a
kelechistack12345 <unachukwukelechi7@gmail.com>
2026-06-06 11:45:12 +0100

    Current Assigment file

--- Assigment/W2/Assigment (2).sql
+++ Assigment/W2/Assigment (2).sql
@@ -149,6 +149,8 @@ JOIN REGION D ON C.REGION_ID = D.ID
 GROUP BY D.NAME
 ORDER BY SALES_AMOUNT DESC;
 
+/*Question 6 — Sales Rep Performance Ranking*/
+
 SELECT 
     C.NAME,
     SUM(a.total_amt_usd) AS SALES_AMOUNT
@@ -161,13 +163,98 @@ JOIN REGION D ON C.REGION_ID = D.ID
 GROUP BY C.NAME
 ORDER BY SALES_AMOUNT DESC;
 
-SELECT
+/*SELECT
     A.GLOSS_QTY,
     A.POSTER_QTY,
     A.STANDARD_QTY
 FROM ORDERS A
 JOIN ACCOUNTS B
-    ON A.ACCOUNT_ID = B.ID;
+    ON A.ACCOUNT_ID = B.ID;*/
+
+/*Identify Inactive Customers*/
+
+SELECT
+    B.NAME,
+    A.OCCURRED_AT,
+    SUM(A.STANDARD_QTY) AS QTY,
+    SUM(A.TOTAL_AMT_USD) AS VALUE
+FROM ORDERS A
+JOIN ACCOUNTS B
+    ON A.ACCOUNT_ID = B.ID
+GROUP BY
+    B.NAME,
+    A.OCCURRED_AT
+HAVING SUM(A.STANDARD_QTY) = 0
+ORDER BY VALUE DESC;
+
+/* Detect High-Value Orders*/
+SELECT 
+    B.NAME,
+    A.STANDARD_QTY
+FROM ORDERS A
+JOIN ACCOUNTS B
+    ON A.ACCOUNT_ID = B.ID
+WHERE A.STANDARD_QTY > 2000
+ORDER BY A.STANDARD_QTY DESC;
+
+/* Customer Purchase Frequency*/
+SELECT 
+    B.NAME,
+    COUNT(A.STANDARD_QTY) AS COUNT_ORDERS
+FROM ORDERS A
+JOIN ACCOUNTS B
+    ON A.ACCOUNT_ID = B.ID
+GROUP BY B.NAME
+ORDER BY COUNT_ORDERS DESC;
+
+
+
+
+
+/*Week 3 Assignment: Business Performance
+Analysis*/
+
+/*Question 1 – Key Revenue Drivers
+Which customers contribute the most revenue to the business, and how frequently do they
+purchase?*/
+
+SELECT
+    B.NAME,
+    COUNT(B.NAME) AS NO_SALES,
+    SUM(A.TOTAL_AMT_USD) AS VALUE
+FROM ORDERS A
+JOIN ACCOUNTS B
+    ON A.ACCOUNT_ID = B.ID
+GROUP BY B.NAME
+ORDER BY VALUE DESC;
+
+/*Question 3 – Regional Business Review*/
+
+SELECT
+    D.NAME,
+    SUM(A.TOTAL_AMT_USD) AS VALUE,
+    COUNT(B.NAME) AS CUSTOMER_ACQUISITION,
+    AVG(A.TOTAL_AMT_USD) AS AVERAGE_ORDER_VALUE
+FROM ORDERS A
+JOIN ACCOUNTS B
+    ON A.ACCOUNT_ID = B.ID
+JOIN SALES_REPS C
+    ON B.SALES_REP_ID = C.ID
+JOIN REGION D
+    ON C.REGION_ID = D.ID
+GROUP BY D.NAME
+ORDER BY AVERAGE_ORDER_VALUE DESC;
+
+/*Question 4 – Strategic Customer Identification*/
+
+
+
+
+
+
+
+
+    
 
 
 
